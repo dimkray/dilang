7187e306e79cb878052c320f15c77e5461e3af50
dimkray <dimkray_home@users.noreply.github.com>
2019-03-06 01:14:20 +0300

    add syntax

--- syntax.py
+++ syntax.py
@@ -1,31 +1,71 @@
 # -*- coding: utf-8 -*-
 Syntons = []
 
-class Synton(object):
-    sType = None
-    sName = None
-    mVars = []
-    mVals = []
-    mOpers = []
-    mSyntons = []
+class OperSynt():
+    sType = None  ##  obj, obj:?, fun, block, obj, method, item, enum
+    sVarName = None
+    mAttr = []
+    sFilter = None ## 2, >1
+    mOperand = []
+    sSpecial = None  ## @, !, \, :
+    sSuff = None     ## !
+    mOperation = []
+    mItem = []
+    cValue = None
+
+    def __init__(self, sTyp, sName = None):
+        self.sType = sTyp
+        self.sVarName = sName
+
+
+class Operation():
+    sType = None  ##  +, -, *, /, %, ^, ~, &, |, >, <, ++, --, &&, ||, <>, ><, ->, ... new
+    iPriority = None
+
+    def __init__(self, sTyp, iPrior):
+        self.sType = sTyp
+        self.iPriority = iPrior
+
+
+class ItemSynt():
+    sType = None  ##  basic: (code, fun:function, class, operand, type, proc:procedure)
+                  ##  stream: (if, else, eisif, case, ifcase, flow, do, while, try, except, for, each)
+                  ##  sub: (code, method, fun:function, class)
+                  ##  type: (unk, bool, int, str, list, dict, coll, enum, date, db, const, block, new...)
+                  ##  subtype: (unk, bool, int, str, list, dict, coll, enum, date, db, const, block, new...)
+                  ##  area: (global, private, ...)
+                  ##  setget: (set, get)
+                  ##  process: (print, del, ...)
+                  ##  proc: (new, ...)
+    sName = None  # (code, fun, method, operand, ...
+    mItem = []
+    mAttr = []
+    mVar = []
+    mOperand = []
+    mOperation = []
 
     def __init__(self, sTyp, sNam):
         self.sType = sTyp
         self.sName = sNam
 
-    def addVal(self, new):
-        self.mVals.append(new)
-
-    def addSynton(self, new):
-        self.mSyntons.append(new)
-
     def printSyn(self):
         print(self.sName, self.sType, self.mVars, self.mVals, self.mSyntons)
 
-print("dima, dim2 = person('Дмитрий', 'Анатольевич', 'Поварницын', age=37, sex=MAN)")
-syn2 = Synton('fun', 'person')
-#syn2.sType = 'fun'
-#syn2.mName = 'person'
+class ValueSynt():
+    sType = None  ##  int, float, str, list, coll, dict, method, ... new
+    mVar = []
+    mVarKey = []
+    mKey = []
+    mValue = []
+
+    def __init__(self, sTyp, mVal = []):
+        self.sType = sTyp
+        self.mValue = mVal
+
+
+print("[dima, dim2] = person('Дмитрий', 'Анатольевич', 'Поварницын', age=37, sex=MAN)")
+syn2 = ItemSynt('basic', 'code')
+
 syn = Synton('simple', None)
 #syn.sType = 'simple'
 syn.mVars = ['dima', 'dim2']
